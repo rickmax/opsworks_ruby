a754460929983c818762d86a099c854a35fdeafd
rickmax <rickmaxg3@hotmail.com>
2017-09-28 16:58:42 -0300

    remove all nodes

--- cookbooks/node/attributes/default.rb
+++
@@ -1,4 +0,0 @@
-default[:node][:revision]      = "HEAD"
-default[:node][:repo_url]      = "https://github.com/joyent/node.git"
-default[:node][:user]          = "nodejs"
-default[:node][:packages]      = []
