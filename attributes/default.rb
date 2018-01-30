1327bb4b5e0e6387132d497f11b55abb6e905771
Henrique <rickmaxg3@hotmail.com>
2018-01-30 15:17:01 -0200

    fix name database

--- attributes/default.rb
+++ attributes/default.rb
@@ -33,7 +33,7 @@ default['defaults']['global']['logrotate_options'] = %w[
 # database
 ## common
 
-default['defaults']['database']['adapter'] = 'postgresql'
+default['defaults']['dbnbdash']['adapter'] = 'postgresql'
 
 # scm
 ## common
